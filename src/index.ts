1c17a941b670cf3af179c6681b36692840eee456
Gits <briangits@proton.me>
2026-06-25 14:31:53 +0300

    chore: format code

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@
  * This is the main entry point. It exports the {@link Logger}, {@link LogLevel},
  * {@link LogRecord}, and built-in log writers.
  */
-export * from './LogLevel'
 export * from './Logger'
+export * from './LogLevel'
 export * from './LogRecord'
 export * from './MaybePromise'
 export * from './writer'
